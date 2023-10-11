2eb1529d971b061a96fc356f1ba306acbd7d2b2f
DonTitoSama <jokonito06@gmail.com>
2023-10-11 15:37:47 +0200

    Create Routes Files, including routes in index.js

---
+++ src/routes/voteRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const voteController = require('../controllers/voteController');
+
+router
+    .route('/music/:id_music/vote')
+    .get(voteController.listAllVote)
+    .post(voteController.createAComment);
+
+router
+    .route('/vote/:id_vote')
+    .get(voteController.getAVote)
+    .put(voteController.updateAVote)
+    .delete(voteController.deleteAVote);
+
+module.exports = router;
