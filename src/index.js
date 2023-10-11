bf01f85103a1c89b13c749fea3c948eed7052474
DonTitoSama <jokonito06@gmail.com>
2023-10-11 13:38:19 +0200

    Connection to mondodb, include library mongoose

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/apinode");
+
+app.use(express.urlencoded());
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
